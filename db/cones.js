2f701865e0b03483ecb91b4e10f1c9d088eafd26
akshat881 <akshatranjan881@gmail.com>
2022-12-17 19:52:04 +0530

    main

--- db/cones.js
+++ db/cones.js
@@ -1,4 +1,13 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://m001-student:TMwehSg4AM3dCtw8@cluster0.azvhtvi.mongodb.net/kito?retryWrites=true&w=majority',()=>{
-    console.log('db done');
+// const mongoose=require('mongoose');
+// mongoose.connect('mongodb+srv://kito45:TMwehSg4AM3dCtw8@cluster0.azvhtvi.mongodb.net/kito?retryWrites=true&w=majority',
+// ()=>{
+//     console.log('db done');
+// })
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://m001-student:YlRcUDSTNeEIm55m@cluster0.azvhtvi.mongodb.net/?retryWrites=true&w=majority",
+{  
+}).then(()=>{
+    console.log("connection done");
+}).catch((e)=>{
+    console.log(e);
 })
